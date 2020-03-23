e3bbb54a469abcb608a75cf23684fee6f5b00dae
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2020-03-23 18:24:18 +0200

    attention script added

---
+++ NMTAttention.py
@@ -0,0 +1,37 @@
+﻿import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+
+def verbose_attention(encoder_state_vectors, query_vector):
+    """A descriptive version of the neural attention mechanism 
+    
+    Args:
+        encoder_state_vectors (torch.Tensor): 3dim tensor from bi-GRU in encoder
+        query_vector (torch.Tensor): hidden state in decoder GRU
+    Returns:
+        
+    """
+    batch_size, num_vectors, vector_size = encoder_state_vectors.size()
+    vector_scores = torch.sum(encoder_state_vectors * query_vector.view(batch_size, 1, vector_size), 
+                              dim=2)
+    vector_probabilities = F.softmax(vector_scores, dim=1)
+    weighted_vectors = encoder_state_vectors * vector_probabilities.view(batch_size, num_vectors, 1)
+    context_vectors = torch.sum(weighted_vectors, dim=1)
+    return context_vectors, vector_probabilities, vector_scores
+
+def terse_attention(encoder_state_vectors, query_vector):
+    """A shorter and more optimized version of the neural attention mechanism
+    
+    Args:
+        encoder_state_vectors (torch.Tensor): 3dim tensor from bi-GRU in encoder
+        query_vector (torch.Tensor): hidden state
+    """
+    vector_scores = torch.matmul(encoder_state_vectors, query_vector.unsqueeze(dim=2)).squeeze()
+    vector_probabilities = F.softmax(vector_scores, dim=-1)
+    context_vectors = torch.matmul(encoder_state_vectors.transpose(-2, -1), 
+                                   vector_probabilities.unsqueeze(dim=2)).squeeze()
+    return context_vectors, vector_probabilities
