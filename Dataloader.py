0a90a3ab906adc76d6467472977e7ccdb499b4b9
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2020-04-01 00:58:01 +0200

    dataloader

---
+++ Dataloader.py
@@ -0,0 +1,11 @@
+﻿import torch.utils.data
+def generate_nmt_batches(dataset, batch_size, shuffle=True, drop_last=True, device="cpu"):    
+    dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size,shuffle=shuffle, drop_last=drop_last)
+    for data_dict in dataloader:
+        lengths = data_dict['x_source_length'].numpy()
+        sorted_length_indices = lengths.argsort()[::-1].tolist()
+        
+        out_data_dict = {}
+        for name, tensor in data_dict.items():
+            out_data_dict[name] = data_dict[name][sorted_length_indices].to(device)
+        yield out_data_dict
